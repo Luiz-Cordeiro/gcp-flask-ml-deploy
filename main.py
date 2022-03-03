443944346eda7d042397c594f9e10ac597787206
Luiz Felipe Vieira Cordeiro <75829130+Luiz-Cordeiro@users.noreply.github.com>
2022-03-03 17:51:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello Google Cloud!'
+    return 'Hello Google Cloud'
 
 @app.route('/echo/<name>')
 def echo(name):
